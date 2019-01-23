632422f08774c0c1636bf2decde962d461853e16
Tarrell Coleman <41527958+tarrellcoleman@users.noreply.github.com>
2019-01-23 14:38:35 -0600

    Created a child componet, BookItem

---
+++ src/components/BookItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <li>{{book.title}}:{{book.author}}</li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'BookItem',
+        props: ['book']
+    }
+</script>
+
+<style>
+    h1, h2 {
+        font-weight: normal
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+</style>
