36a388efa9a843e538226ebb62649a5d2e12993a
Tarrell Coleman <41527958+tarrellcoleman@users.noreply.github.com>
2019-01-23 15:28:19 -0600

    Typos and bookform was completed

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,7 +1,5 @@
 <template>
-    <div>
-        <li>{{book.title}}:{{book.author}}</li>
-    </div>
+    <li>{{book.title}}:{{book.author}}</li>
 </template>
 
 <script>
@@ -12,13 +10,6 @@
 </script>
 
 <style>
-    h1, h2 {
-        font-weight: normal
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
     li {
         display: block;
         margin: 0 10px;
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,18 +4,22 @@
         <ul>
             <book-item v-for="book in books" :book='book'></book-item>
         </ul>
+        <hr>
+        <book-form @addBook='appendBook'></book-form>
     </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
+
     export default {
         name: 'BookList',
         components: {
-            BookItem
+            BookItem,
+            BookForm
         },
-        data()
-        {
+        data() {
             return {
                 title: 'All Books',
                 books: [
@@ -23,6 +27,11 @@ import BookItem from './BookItem';
                     {title: 'American Gods', author: 'Neil Gaiman'},
                     {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
                 ]
+            }
+        },
+        methods: {
+            appendBook(bookTitle, bookAuthor) {
+                this.books.push({title: bookTitle, author: bookAuthor})
             }
         }
     }
