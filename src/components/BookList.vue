df93cae80e268c584b35d3fd494b732e0fe5f9c1
Tarrell Coleman <41527958+tarrellcoleman@users.noreply.github.com>
2019-01-23 14:29:45 -0600

    A for loop that lists books and authors was added

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">
+                {{book.title}}:{{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -10,7 +14,12 @@
         data()
         {
             return {
-                title: 'All Books'
+                title: 'All Books',
+                books: [
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
             }
         }
     }
@@ -19,5 +28,13 @@
 <style>
     h1, h2 {
         font-weight: normal
+    }
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
